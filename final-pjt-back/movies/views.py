d44137473e7adda0bbd1f9ec203a4b8b02b95fe0
gusdud2068 <gusdud2068@gmail.com>
2022-11-16 13:43:00 +0900

    edit

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Movie, Genre
+from .serializers import MovieListSerializer, GenreSerializer
+
 
 # Create your views here.
+@api_view(["GET"])
+def index(request):
+    if request.method == "GET":
+        movies=Movie.objects.all()
+        serializer=MovieListSerializer(movies,many=True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
